05a10274412d4b2980f4b3c5251373ec0f5ee6db
TienThan <thantien07122000@gmail.com>
2021-11-11 22:58:29 +0700

    FirstCommit

---
+++ lib/util/api_key.dart
@@ -0,0 +1,1 @@
+const String apiKey = 'd2099c3d07ad4235a852cf3f0efaab3c';
---
+++ lib/util/topics.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_news/models/topic.dart';
+
+List<Topic> topics = [
+  Topic('Business', 'https://microad.vn/wp-content/uploads/2020/02/BDM.jpg'),
+  Topic('Entertainment',
+      'http://vieclam43.net/uploads/880005-04-2021_071036.jpg'),
+  Topic('General',
+      'https://indianmarketview.com/wp-content/uploads/News-Papers-300x200.jpeg'),
+];
